a112cc73bc5971b6ea7c4d9005fec7e25b7eda29
Ranjit Shrestha <rangeetshrestha12@gmail.com>
2025-05-18 12:08:45 +0545

    day 5 done

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -3,48 +3,67 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
+        appBar: AppBar(
+          leading: Icon(Icons.home_outlined),
+          centerTitle: true,
+          title: Text("Trending"),
+          actions: [Icon(Icons.more_vert)],
+        ),
 
-            SizedBox(height: 40),
-
-            Center(
-              child: Text(
-                "Hello World",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.w800,
-                  color: Colors.white,
-                  backgroundColor: Colors.black,
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              color: Colors.yellow,
-              width: 200,
-              height: 200,
-            ),
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              NewsCard(),
+              NewsCard(),
+              NewsCard(),
+              NewsCard(),
+            ],
+          ),
         ),
       ),
     ),
   );
 }
 
+class NewsCard extends StatelessWidget {
+  const NewsCard({super.key});
 
-// FLutter Project create
-// runApp
-// MaterialApp
-// Scaffold
-// Container
-// Text
-// SizedBox
-// Column
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              "images/image.jpg",
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Russian warship: Moskva sinks in Black Sea Black Sea",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Icon(Icons.schedule),
+              SizedBox(width: 10),
+              Text("4hr ago"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
